9889c7d722561922db6077650cf55aab1952890e
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-07 19:38:15 +0100

    Added function to take care of station dropdown menu

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
-// noprotect
 var mta = {
-  'n': ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
-  'l': ['8th-l', '6th', 'us', '3rd', '1st'],
-  's': ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
+  'Line N': ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
+  'Line L': ['8th-l', '6th', 'us', '3rd', '1st'],
+  'Line 6': ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
 };
 
-function menu() {
-  var response = prompt('Welcome to MTA!!! \n(m)ta or (q)uit');
-  while(response !== 'q') {
-    var userInput = getUserInput();
-    var tripLength = calculateStops(userInput);
-    alert('Your trip length is ' + tripLength + ' stops!');
-  }
-}
+// function recordInput() {
 
-function getUserInput() {
-  var startTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-  var firstStop = prompt('Which stop: ' + mta[startTrain].join(', '));
-  var stopTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-  var lastStop = prompt('Which stop: ' + mta[stopTrain].join(', '));
-  return {startTrain: startTrain, 
-          firstStop: firstStop,
-          stopTrain: stopTrain,
-          lastStop: lastStop};
-}
+  // $('#L').on('change', function() {
+  //   startTrain = $(this).val();
+  //   alert(startTrain);
+  // });
 
-function calculateStops(userInput) {
-  return userInput.startTrain === userInput.stopTrain ? sameLine(userInput) : differentLines(userInput);
-}
+  // startTrain = $('.initial-train').val();
 
-function differentLines(userInput) {
-  var intersection = mta[userInput.startTrain].filter(function(stop) {
-      return mta[userInput.stopTrain].indexOf(stop) != -1;
-    })[0];
+  // if (startTrain === 'Line N') {
+  //   alert('you clicked on line n')
+  //   for (i = 1; i < mta[startTrain.length]; i++) {
+  //     $('.initial-stop').append('<option>' + i + '</option>')
+  //   } 
+  // }
 
-  var startTrainIndex = mta[userInput.startTrain].indexOf(userInput.firstStop);
-  var startTrainIntersectionIndex = mta[userInput.startTrain].indexOf(intersection);
-  var firstTripLength = Math.abs(startTrainIndex - startTrainIntersectionIndex);
+// }
 
-  var stopTrainIndex = mta[userInput.stopTrain].indexOf(userInput.lastStop);
-  var stopTrainIntersectionIndex = mta[userInput.stopTrain].indexOf(intersection);
-  var lastTripLength = Math.abs(stopTrainIndex - stopTrainIntersectionIndex);
 
-  return firstTripLength + lastTripLength;
-}
+$(document).ready(function() {
+  // recordInput();
+  var startTrain = $('select[name=selector]').val();
 
-function sameLine(userInput) {
-  return Math.abs(mta[userInput.startTrain].indexOf(userInput.firstStop) - mta[userInput.stopTrain].indexOf(userInput.lastStop));
-}
 
-menu();
+  $('select[name=selector]').on('change', function() {
+    startTrain = $(this).val();
+    if (startTrain === 'Line N') {
+      for (i = 0; i < mta[startTrain].length; i++) {
+        $('.initial-stop').append('<option>' + mta[startTrain][i] + '</option>');
+      }
+    } else if (startTrain === 'Line L') {
+      for (i = 0; i < mta[startTrain].length; i++) {
+        $('.initial-stop').append('<option>' + mta[startTrain][i] + '</option>');
+      }
+    } else if (startTrain === 'Line 6') {
+      for (i = 0; i < mta[startTrain].length; i++) {
+        $('.initial-stop').append('<option>' + mta[startTrain][i] + '</option>');
+      }
+    }
+  })
+
+
+})
 
